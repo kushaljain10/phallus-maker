3c63f33bd51cf60e4cb43f6efe58305ea7149829
Kushal Jain <jain.kushal10@gmail.com>
2023-06-18 19:53:00 +0530

    add phallus maker code and files

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const fs = import('fs');
+import {generateBro} from './generate.js';
+// import {fetchCSVData} from './random_generator.js';
+import express from 'express';
+import cors from 'cors';
+const app = express();
 const http = require('http');
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
+
+app.use(cors());
+
+app.get('/generate', generateBro)
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
